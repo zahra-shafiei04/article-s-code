3addfd3cfd9eebcac5217125bf2469d1eed885f3
zahra-shafiei04 <137926508+zahra-shafiei04@users.noreply.github.com>
2023-09-21 01:26:34 -0400

    Update final-optimized-code-selection.py

--- final-optimized-code-selection.py
+++ final-optimized-code-selection.py
@@ -9,68 +9,97 @@ start_time = time.time()
 
 def Wright_Fisher_model(N, p0, generations, mu, v):
     
-    # Initialize variable to store the final allele frequency
-    f_final = None  
-
     # Initialize 'p' as an array with a desired length and set all elements to 'p0':
-    p = np.full(100000, p0)
-
+    p = np.full(100 * N, p0)
+    
     for jj in range(generations):
         
         # Describing mutations all over the simulation and fluctuating selection:
-        s = np.random.normal(0, v)
-        t = np.random.normal(0, v)
-        
+        s = np.random.normal(0, v) #s = sigma
+        t = np.random.normal(0, v) #t = tau
+
         # Update 'p' using the given equations, based on the previous value of 'p':
-        p = p + mu * (1 - (2 * p)) + (p * (1 - p) * (s - t)) / (1 + (p * s) + (t * (1 - p)))
-        
-        # Clip the values to ensure they stay within [0, 1]:
-        p = np.clip(p, 0, 1)
+        p = p + mu * (1 - p) +  (p * (1 - p) * (s - t)) / (1 + (p * s) + (t * (1 - p)))
         
         # Describing drift:
         allele_counts = np.random.binomial(2 * N, p)
         p = allele_counts / (2. * N)
         
-        # Store the final frequency after these two steps:
-        f_final = p
+        p[(p==1)]=0
         
-    return f_final
+    return p 
 
-N = 100
+N = 1000
 p0 = 0.01
-generations = 1000
-mu = 1e-3
-v_values = [1e-10,1e-5, 1e-1, 1, 10]  
-
-
-# Collect the final allele frequencies for different 'v' values
-all_final_frequencies = [Wright_Fisher_model(N, p0, generations, mu, v) for v in v_values]
+generations = 10 * N
+mu = 1 / (10 * N)
+v_values = [1e-2, 5e-2, 2e-2, 1e-1]  
 
 
 #%%
+#saving data:
+# Collect the final allele frequencies for different 'v' values
+all_final_frequencies = [Wright_Fisher_model(N, p0, generations, mu, v) for v in v_values]
+
+#%%
+#defining the analytical solution function:
+def r1(B):
+    return (1 - (np.sqrt(1 + (4 / B)))) / 2
+
+def r2(B):
+    return (1 + (np.sqrt(1 + (4 / B)))) / 2
+
+def k(B):
+    return np.log(((1 - r1(B)) / (-r1(B))) * (r2(B) / (r2(B) - 1)))
+
+def g(y, B):
+    return np.log(((1 - r1(B)) / (y - r1(B))) * ((r2(B) - y) / (r2(B) - 1)))
+
+def f1(y, B):
+    return (2 / (k(B) * y * (1 - y))) * g(y, B)
+
+#%%
+color_cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
 # Plotting the SFS for each 'v' value in one figure
 for i, v in enumerate(v_values):
+
+    # Create a histogram with the final frequency data and specified bin width
+    bin_width = np.linspace(((1 / N) + (1 /(2 * N))), 1, 100)
     
-    # Create a histogram with only the final frequency for each 'v' value
-    counts, bins = np.histogram(all_final_frequencies[i], bins=101)
-    
-    # Get the bin centers
+    counts, bins = np.histogram(all_final_frequencies[i], bins=bin_width)
+
+    # Calculate the bin centers
     bin_centers = (bins[:-1] + bins[1:]) / 2
-    
+
     # Calculate the Riemann sum to estimate the area under the curve
     riemann_sum = np.sum(counts * (bin_centers[1] - bin_centers[0]))
     
-    #normalizing the curve
+    # Normalizing the curve
     normalized_counts = counts / riemann_sum
     
-    print(f"Area under curve for v={v}: {np.sum(normalized_counts) * (bin_centers[1] - bin_centers[0])}")
+    color = color_cycle[i % len(color_cycle)]
     
-    # Create the line plot
-    plt.plot(bin_centers, normalized_counts, label=f'v={v}')
-     
+    print(f"Area under simulation curve for v={v}: {np.sum(normalized_counts) * (bin_centers[1] - bin_centers[0])}")
+    
+    plt.plot(bin_centers, normalized_counts, label=f'Simulated v={v}')
+       
+    #same process for the second function:
+    B = 2 * N * 2 * v
+    
+    f1_values = f1(bin_centers, B)  
+    
+    riemann_sum_analytical = np.sum(f1_values * (bin_centers[1] - bin_centers[0]))
+    
+    normalized_curve = f1_values / riemann_sum_analytical
+    
+    print(f"Area under analytical solution curve for v={v}:{np.sum(normalized_curve) * (bin_centers[1] - bin_centers[0])}")
+    
+    plt.plot(bin_centers, normalized_curve, linestyle='--', label=f'Analytical v={v}', color=color)
+    
 plt.xlabel('Frequency')
-plt.ylabel('Counts')
-plt.title('SFS Fluctuating Selection')
+plt.ylabel('Normalized Counts / Normalized Analytical Values')
+plt.title('SFS Fluctuating Selection & Normalized Analytical Solution')
 plt.legend()
 plt.show()
 
@@ -81,12 +110,6 @@ end_time = time.time()
 # Calculate the total running time
 running_time = end_time - start_time
 print("Total running time:", running_time, "seconds")
-
-
-
-
-
-
 
 
 
